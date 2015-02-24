6d739f6d329520b3c0e835d8a3d020fa0f5f5e9a
Jolanta Wójcik <s175371@v-ie.uek.krakow.pl>
2015-02-24 23:54:15 +0100

    commit public

---
+++ public/templates/logged.php
@@ -0,0 +1,7 @@
+<div class="accountbox"><p>Hello <a href="#"><?php echo escape($user->data()->login); ?></a>!</p>	
+<ul>
+	<li><a href="/mymvc/public/logout">Log out</a></li>
+	<li><a href="/mymvc/public/update">Update</a></li>
+	<li><a href="/mymvc/public/changepassword">Change password</a></li>
+</ul>
+</div>
